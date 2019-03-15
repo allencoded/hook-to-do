046a576953858d1f9a72a26205a096173891f6bf
Allen Hendricks <allencoded@gmail.com>
2019-03-14 20:52:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-class App extends Component {
-  render() {
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function App() {
+  const [ todos, setTodos ] = useState([
+    { text: 'learn abut some hooks yo' },
+    { text: 'call the cable company ask for more internets' },
+    { text: 'make a new friend' },
+  ]);
+
+  function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
     );
   }
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const renderTodos =() => todos.map((todo, i) => (
+    <Todo
+      key={i}
+      index={i}
+      todo={todo}
+    />
+  ));
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        { renderTodos() }
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
